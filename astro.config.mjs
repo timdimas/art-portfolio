3ce1d4370e22b3f006222a7deb7bd45e4d7ec909
edimas <dimasefthymios@gmail.com>
2026-09-01 15:07:05 +0300

    update tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwindcss()],
 	server: {
 		host: true,
 	},
